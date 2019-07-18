87a859c9ceedcdcc54df80d7221eebc06c26d3e5
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2019-07-18 13:18:21 +0200

    Add node 12.6 dockerfile

--- node/frontend/build.sh
+++ node/frontend/build.sh
@@ -3,4 +3,4 @@
 ##  Get the directory that this script is in and move there
 cd "$( dirname "$0" )"
 
-exec docker build -t kariusdx/node-frontend:6.9 .
+exec docker build -f node6.Dockerfile -t kariusdx/node-frontend:6.9 .
